e48048911368b092844a031cc4f2615bb0227037
Mark Meves <mark.meves@gmail.com>
2011-04-16 18:41:14 -0400

    Add all these goddam files i don't know
    this is rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Current::Application.config.secret_token = '8b396c087d23c500052e94d12e4cab0900f72e002d7df161a16d9c79ea6174aeb04973736cae64c2a09057a2c25f4781b3603011536ad7abe02c494b6e4d9ca2'
