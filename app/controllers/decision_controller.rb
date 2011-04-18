f53896074593a0f602ad02abcd2c7d5bf5e86a3a
Mark Meves <mark.meves@gmail.com>
2011-04-18 16:09:28 -0400

    (controllers for voters and votes)

--- app/controllers/decision_controller.rb
+++ app/controllers/decision_controller.rb
@@ -4,9 +4,26 @@ class DecisionController < ApplicationController
   end
 
   def create
+    @vote = Vote.new(params[:vote]) # :token
+
+    respond_to do |format|
+      if @vote.save
+        # format.html { redirect_to(@vote, :notice => 'Vote was successfully created.') }
+        format.html do redirect_to('/',
+          :notice => "One #{@vote.name} vote was successfully cast for " <<
+            "voting token \"#{@vote.token}\".")
+        end
+        # format.xml  { render :xml => @vote, :status => :created, :location => @vote }
+      else
+        # format.html { render :action => "new" }
+        format.html { render('bylaws') }
+        # format.xml  { render :xml => @vote.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   def bylaws
+    # just show static view for now
   end
 
 end
